9b817bf6f790ff4bfde8186771d29985c1eabce4
Anton Vorontsov <williams@ozon.ru>
2020-02-04 11:38:29 +0300

    Added summaries for new models. Removed some internals.

--- src/RabbitMQ.Client.Core.DependencyInjection/Configuration/RabbitMqConnectionOptions.cs
+++ src/RabbitMQ.Client.Core.DependencyInjection/Configuration/RabbitMqConnectionOptions.cs
@@ -1,9 +1,19 @@
 namespace RabbitMQ.Client.Core.DependencyInjection.Configuration
 {
+    /// <summary>
+    /// An options model that "contains" sections for producing and consuming connections of a RabbitMQ clients
+    /// <see cref="IQueueService"/>, <see cref="IConsumingService"/> and <see cref="IProducingService"/>.
+    /// </summary>
     public class RabbitMqConnectionOptions
     {
+        /// <summary>
+        /// Producer connection.
+        /// </summary>
         public RabbitMqClientOptions ProducerOptions { get; set; }
         
+        /// <summary>
+        /// Consumer connection.
+        /// </summary>
         public RabbitMqClientOptions ConsumerOptions { get; set; }
     }
 }
