d1b8cb7caa8af969383b2dbc24d785bb02fa048c
Anton Vorontsov <williams@ozon.ru>
2021-05-12 04:24:44 +0500

    Auto-ack fixes.

--- src/RabbitMQ.Client.Core.DependencyInjection/Models/MessageHandlingContext.cs
+++ src/RabbitMQ.Client.Core.DependencyInjection/Models/MessageHandlingContext.cs
@@ -5,14 +5,17 @@ namespace RabbitMQ.Client.Core.DependencyInjection.Models
 {
     public class MessageHandlingContext
     {
-        public MessageHandlingContext(BasicDeliverEventArgs message, Action<BasicDeliverEventArgs>? ackAction)
+        public MessageHandlingContext(BasicDeliverEventArgs message, Action<BasicDeliverEventArgs> ackAction, bool disableAutoAck)
         {
             Message = message;
             AckAction = ackAction;
+            AutoAckEnabled = !disableAutoAck;
         }
 
         public BasicDeliverEventArgs Message { get; }
         
-        public Action<BasicDeliverEventArgs>? AckAction { get; }
+        public Action<BasicDeliverEventArgs> AckAction { get; }
+        
+        public bool AutoAckEnabled { get; }
     }
 }
